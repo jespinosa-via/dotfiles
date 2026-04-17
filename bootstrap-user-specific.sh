6bb82304c697e8b15cc2051e6248475c66517959
Jake Espinosa <jespinosa@viascience.com>
2026-04-17 15:57:11 +0000

    initial config

--- bootstrap-user-specific.sh
+++ bootstrap-user-specific.sh
@@ -1,7 +1,6 @@
 #!/bin/env bash
 # set -e
 
-
-echo "no customizations yet"
-echo "If you need custom startup commands to run on workspace start, place them in bootstrap-user-specific.sh in the dotfiles repo"
-
+brew update
+brew upgrade
+brew install kubectl argocd helm
