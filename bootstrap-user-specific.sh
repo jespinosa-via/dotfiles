b0c76792f3a6a0c800d9599062a42166405200e6
Jake Espinosa <jespinosa@viascience.com>
2026-04-21 08:43:44 -0600

    fnm

--- bootstrap-user-specific.sh
+++ bootstrap-user-specific.sh
@@ -1,6 +1,11 @@
 #!/bin/env bash
 # set -e
 
+# install tools
 brew update
 brew upgrade
-brew install kubectl argocd helm
+brew install kubectl argocd helm fnm
+
+# set node env
+fnm install --lts
+fnm use --lts
