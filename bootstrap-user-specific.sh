e313abde24f13f16e38fbe1a64bab896e812feaf
Jake Espinosa <jespinosa@viascience.com>
2026-06-30 14:46:04 -0600

    add zsh

--- bootstrap-user-specific.sh
+++ bootstrap-user-specific.sh
@@ -4,10 +4,19 @@
 # install tools
 brew update
 brew upgrade
-brew install kubectl argocd helm fnm zsh-completions
+brew install kubectl argocd helm fnm zsh
+
+# set zsh as default shell
+if ! grep -q "$(brew --prefix)/bin/zsh" /etc/shells; then
+    echo "$(brew --prefix)/bin/zsh" | sudo tee -a /etc/shells
+fi
+chsh -s "$(brew --prefix)/bin/zsh"
 
 # set node env
 fnm install --lts
 fnm default lts-latest
 eval "$(fnm env --use-on-cd)"
 fnm use lts-latest
+
+# launch zsh
+exec "$(brew --prefix)/bin/zsh"
